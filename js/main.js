3acfaed83e26cd0b8468959a30932847c9670d7f
unknown <dungmincb@gmail.com>
2018-10-19 02:29:55 +0700

    as

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.selectpicker1').selectpicker();
+  $('.carousel').carousel();
+  var x = $('.banner-img > img').height();
+  $('.banner-wrapper').css('height', x);
+});
