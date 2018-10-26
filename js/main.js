de21e162d7d33d45be2633b37482208dbcd748ea
unknown <dungmincb@gmail.com>
2018-10-27 01:59:34 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   $('.selectpicker1').selectpicker();
   // $('#carouselExampleIndicators').carousel();
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+  });
   var x = $('.banner-img > img').height();
   $('.banner-wrapper').css('height', x);
 });
